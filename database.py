bfd7cbba0b62c9ae33a2bb246860ece29170a67d
Uğur Baştuğ <bastugugur85@gmail.com>
2025-08-13 13:46:08 +0300

    fix attempt for deployment 1

--- database.py
+++ database.py
@@ -4,13 +4,16 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 load_dotenv()
+
 def get_connection():
     conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT", 5432),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+)
+
     return conn
 
 
