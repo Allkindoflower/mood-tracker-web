21c91bd50dadd3b66efcb78945b03cf14911355e
Uğur Baştuğ <bastugugur85@gmail.com>
2025-08-13 13:54:24 +0300

    refactor: change get connection function to work locally and on Render

--- database.py
+++ database.py
@@ -6,14 +6,20 @@ from psycopg2.extras import RealDictCursor
 load_dotenv()
 
 def get_connection():
-    conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT", 5432),
-    database=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD")
-)
+    
+    host = os.getenv("DB_HOST", "localhost")
+    port = os.getenv("DB_PORT", 5432)
+    database = os.getenv("DB_NAME", "moods_db")
+    user = os.getenv("DB_USER", "bastu")
+    password = os.getenv("DB_PASSWORD", "123")
 
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password
+    )
     return conn
 
 
